0b449db76caeb58da7331ee8d3115dda8d081259
Meghan Nelson <meghan@meghan.net>
2014-06-14 13:31:37 -0400

    can turn button 1 on and off

--- gen/com/example/lightsout/R.java
+++ gen/com/example/lightsout/R.java
@@ -10,20 +10,33 @@ package com.example.lightsout;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int off=0x7f040001;
+        public static final int on=0x7f040000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int TableLayout01=0x7f060000;
-        public static final int TableRow01=0x7f060001;
+        public static final int TableLayout01=0x7f070000;
+        public static final int TableRow01=0x7f070001;
+        public static final int button1=0x7f070002;
+        public static final int button2=0x7f070003;
+        public static final int button3=0x7f070004;
+        public static final int button4=0x7f070005;
+        public static final int button5=0x7f070006;
+        public static final int button6=0x7f070007;
+        public static final int button7=0x7f070008;
+        public static final int button8=0x7f070009;
+        public static final int button9=0x7f07000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int grid_layout=0x7f030001;
+        public static final int buttons=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
     }
     public static final class style {
         /** 
@@ -47,10 +60,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- src/com/example/lightsout/MainActivity.java
+++ src/com/example/lightsout/MainActivity.java
@@ -4,12 +4,45 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	public Button button1, button2, button3, button4, button5, button6, button7, button8, button9;
+	public boolean button1_on, button2_on, button3_on, button4_on, button5_on, button6_on, button7_on, button8_on, button9_on;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.grid_layout);
+		setContentView(R.layout.buttons);
+		
+		button1 = (Button) findViewById(R.id.button1);
+		button2 = (Button) findViewById(R.id.button2);
+		button3 = (Button) findViewById(R.id.button3);
+		button4 = (Button) findViewById(R.id.button4);
+		button5 = (Button) findViewById(R.id.button5);
+		button6 = (Button) findViewById(R.id.button6);
+		button7 = (Button) findViewById(R.id.button7);
+		button8 = (Button) findViewById(R.id.button8);
+		button9 = (Button) findViewById(R.id.button9);
 	}
+	
+	public void onClick(View v) {
+		if(v.getId() == R.id.button1) {
+			//Light is on, time to turn it off
+			if(button1_on) {
+				button1.setBackgroundColor(getResources().getColor(R.color.off));		
+				button1_on = false;
+			} else {
+			//Light is off, time to turn it on
+				button1.setBackgroundColor(getResources().getColor(R.color.on));	
+				button1_on = true;
+			}
+
+		}
+	}
+	
+
+	
 }
